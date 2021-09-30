4a5aac2007fee4a5acf895c4ed6a0595a8771a9d
pahntanapat <pahntanapat@gmail.com>
2021-09-30 09:10:01 +0000

    add src

---
+++ src/unified.py
@@ -0,0 +1,37 @@
+import logging
+from loguru import logger
+from gunicorn.glogging import Logger
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth,
+                   exception=record.exc_info).log(level, record.getMessage())
+
+
+class StubbedGunicornLogger(Logger):
+    def setup(self, cfg):
+        self.loglevel = self.LOG_LEVELS.get(cfg.loglevel.lower(), logging.INFO)
+
+        handler = logging.NullHandler()
+
+        self.error_logger = logging.getLogger("gunicorn.error")
+        self.error_logger.addHandler(handler)
+
+        self.access_logger = logging.getLogger("gunicorn.access")
+        self.access_logger.addHandler(handler)
+
+        self.error_logger.setLevel(self.loglevel)
+        self.access_logger.setLevel(self.loglevel)
