698622c7764d7a0799bc1b75996e031d3e795036
pahntanapat <pahntanapat@gmail.com>
2021-09-30 11:16:03 +0000

    example

--- example.py
+++ example.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 import logging
+from loguru import logger
+from sys import argv
 from os import cpu_count, environ
 from fastapi import FastAPI, Response
-from fastapi.responses import ORJSONResponse
 from unified_api_log.gunicorn import global_config, Gunicorn
 
-DEBUG = not (environ.get('DEBUG', False)
+DEBUG = not (environ.get('DEBUG', argv[1] if (len(argv) > 1) else False)
              in {0, '0', '', None, False, 'false', 'False'})
 
 app = FastAPI(
@@ -13,21 +14,22 @@ app = FastAPI(
     title='RESUME: The Medical Speech-to-Text API',
     description=
     'Convert medical speech either conversation or dictation to medical record document',
-    default_response_class=ORJSONResponse,
-    version='0.1.0')
+    version='0.0.1')
 
 
-@app.get('/test')
+@app.get('/')
 def test(response: Response):
     response.set_cookie('start', datetime.now().isoformat(), max_age=3600)
-    return {'response_time': datetime.now().isoformat()}
+    return {'response_time': datetime.now().isoformat(), 'debug': DEBUG}
 
 
 if __name__ == '__main__':
-    logger = global_config(log_level=(logging.DEBUG if
-                                      (DEBUG) else logging.INFO),
-                           json=(not DEBUG))
+    global_config(log_level=(logging.DEBUG if (DEBUG) else logging.INFO),
+                  json=(not DEBUG))
+    logger.info('Debug mode: {}'.format(DEBUG))
+
     cpu = cpu_count() * 2
+    logger.info('Use CPU Workers = {}'.format(cpu))
     Gunicorn(app, {
         "bind": "0.0.0.0:80",
         "workers": cpu,
